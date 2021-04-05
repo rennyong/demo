1fc973a96f87a91942a57d8f5c4cd86c8d8ae4da
rennyong <rennyong@gmail.com>
2021-04-05 21:00:00 +0800

    Initial Commit

---
+++ src/main/java/com/example/demo/model/demoEntity.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class demoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    
+}
